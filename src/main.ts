c876853c40e2d2e2e493b1970ff6c1acab872ac0
Dmitry Marov <d.marov94@gmail.com>
2024-08-27 19:53:22 +0300

    updated demo

--- src/main.ts
+++ src/main.ts
@@ -12,40 +12,39 @@ const canvas = new Canvas(canvasElement, {
 
 function createNodeElement(name: string): [HTMLElement, HTMLElement, HTMLElement] {
     const node =  document.createElement('div');
-    const text =  document.createElement('div');
-
     node.classList.add("node");
-    node.appendChild(text);
-
-    text.innerText = name;
 
     const frontPort = document.createElement('div');
-    node.prepend(frontPort);
+    node.appendChild(frontPort);
+
+    const text =  document.createElement('div');
+    text.innerText = name;
+    node.appendChild(text);
 
     const backPort = document.createElement('div');
     node.appendChild(backPort);
 
-    return [node, frontPort, backPort];
+    return [frontPort, node, backPort];
 }
 
-const node1 = createNodeElement("Node 1");
-const node2 = createNodeElement("Node 2");
-const node3 = createNodeElement("Node 3");
-const node4 = createNodeElement("Node 4");
+const [port11, node1, port12] = createNodeElement("Node 1");
+const [port21, node2, port22] = createNodeElement("Node 2");
+const [port31, node3, port32] = createNodeElement("Node 3");
+const [port41, node4, port42] = createNodeElement("Node 4");
 
 canvas
-    .addNode({ id: "node-1", element: node1[0], x: 200, y: 400 })
-    .markPort({ id: "port-1-1", element: node1[1], nodeId: "node-1" })
-    .markPort({ id: "port-1-2", element: node1[2], nodeId: "node-1" })
-    .addNode({ id: "node-2", element: node2[0], x: 600, y: 500 })
-    .markPort({ id: "port-2-1", element: node2[1], nodeId: "node-2" })
-    .markPort({ id: "port-2-2", element: node2[2], nodeId: "node-2" })
-    .addNode({ id: "node-3", element: node3[0], x: 200, y: 800 })
-    .markPort({ id: "port-3-1", element: node3[1], nodeId: "node-3" })
-    .markPort({ id: "port-3-2", element: node3[2], nodeId: "node-3" })
-    .addNode({ id: "node-4", element: node4[0], x: 1000, y: 600 })
-    .markPort({ id: "port-4-1", element: node4[1], nodeId: "node-4" })
-    .markPort({ id: "port-4-2", element: node4[2], nodeId: "node-4" })
+    .addNode({ id: "node-1", element: node1, x: 200, y: 400 })
+    .markPort({ id: "port-1-1", element: port11, nodeId: "node-1" })
+    .markPort({ id: "port-1-2", element: port12, nodeId: "node-1" })
+    .addNode({ id: "node-2", element: node2, x: 600, y: 500 })
+    .markPort({ id: "port-2-1", element: port21, nodeId: "node-2" })
+    .markPort({ id: "port-2-2", element: port22, nodeId: "node-2" })
+    .addNode({ id: "node-3", element: node3, x: 200, y: 800 })
+    .markPort({ id: "port-3-1", element: port31, nodeId: "node-3" })
+    .markPort({ id: "port-3-2", element: port32, nodeId: "node-3" })
+    .addNode({ id: "node-4", element: node4, x: 1000, y: 600 })
+    .markPort({ id: "port-4-1", element: port41, nodeId: "node-4" })
+    .markPort({ id: "port-4-2", element: port42, nodeId: "node-4" })
     .connectPorts({ id: "con-1", from: "port-1-2", to: "port-2-1" })
     .connectPorts({ id: "con-2", from: "port-3-2", to: "port-2-1" })
     .connectPorts({ id: "con-3", from: "port-2-2", to: "port-4-1" });
