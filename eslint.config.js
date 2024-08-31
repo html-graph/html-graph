e1ab7634d4a6eea5fe0e4f0314558f1722572df6
Dmitry Marov <d.marov94@gmail.com>
2024-08-31 16:28:23 +0300

    fixed lint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,13 @@ import globals from "globals";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config({
-  files: ["**/*.{js,mjs,cjs,ts}"],
-  languageOptions: { globals: globals.browser },
-  ignores: ["dist/**/*"],
-  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
-});
+export default tseslint.config(
+  {
+    ignores: ["dist/**/*"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+    extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  },
+);
