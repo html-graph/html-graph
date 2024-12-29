bf791c302cea7e1ff6bb543d18ccf1cf6054ce22
Dmitry Marov <d.marov94@gmail.com>
2024-12-29 17:47:25 +0300

    rounded path wip

--- lib/edges/utils/create-rounded-path/create-rounded-path.spec.ts
+++ lib/edges/utils/create-rounded-path/create-rounded-path.spec.ts
@@ -1,9 +1,51 @@
+import { Point } from "../../point";
 import { createRoundedPath } from "./create-rounded-path";
 
 describe("createRoundedPath", () => {
-  it("should return empty string for empty array", () => {
-    const res = createRoundedPath([]);
+  it("should return empty string for path win no dots", () => {
+    const res = createRoundedPath([], 0, 0);
 
     expect(res).toEqual("");
   });
+
+  it("should return dot for path with no lines", () => {
+    const res = createRoundedPath([[0, 0]], 0, 0);
+
+    expect(res).toEqual("M 0 0");
+  });
+
+  it("should return line for path with no angles", () => {
+    const path: Point[] = [
+      [0, 0],
+      [0, 15],
+    ];
+
+    const res = createRoundedPath(path, 0, 0);
+
+    expect(res).toEqual("M 0 0 L 0 15");
+  });
+
+  it("should return rounded line for three items array", () => {
+    const path: Point[] = [
+      [0, 0],
+      [30, 0],
+      [30, 30],
+    ];
+
+    const res = createRoundedPath(path, 10, 0);
+
+    expect(res).toEqual("M 0 0 L 20 0 C 30 0 30 0 30 10 L 30 30");
+  });
+
+  // it("should limit roundness to half joint distance", () => {
+  //   const path: Point[] = [
+  //     [0, 0],
+  //     [30, 0],
+  //     [30, 30],
+  //   ];
+
+  //   const res = createRoundedPath(path, 100, 0);
+
+  //   expect(res).toEqual("M 0 0 L 0 0 C 30 0 30 0 30 30 L 30 30");
+  // });
 });
--- lib/edges/utils/create-rounded-path/create-rounded-path.ts
+++ lib/edges/utils/create-rounded-path/create-rounded-path.ts
@@ -1,5 +1,66 @@
 import { Point } from "../../point";
 
-export const createRoundedPath: (ps: readonly Point[]) => string = () => {
-  return "";
+export const createRoundedPath: (
+  ps: readonly Point[],
+  roundness: number,
+  arrowGap: number,
+) => string = (path: readonly Point[], roundness: number, arrowGap: number) => {
+  const parts: string[] = [];
+  console.log(arrowGap);
+
+  if (path.length > 0) {
+    parts.push(`M ${path[0][0]} ${path[0][1]}`);
+  }
+
+  if (path.length === 2) {
+    parts.push(`L ${path[1][0]} ${path[1][1]}`);
+  }
+
+  if (path.length > 2) {
+    const last = path.length - 1;
+    let dxn = 0;
+    let dyn = 0;
+    let distanceNext = 0;
+
+    path.forEach((p, i) => {
+      let dxp = 0;
+      let dyp = 0;
+      let distancePrev = 0;
+
+      if (i > 0) {
+        dxp = -dxn;
+        dyp = -dyn;
+        distancePrev = distanceNext;
+      }
+
+      if (i < last) {
+        const next = path[i + 1];
+        dxn = next[0] - p[0];
+        dyn = next[1] - p[1];
+        distanceNext = Math.sqrt(dxn * dxn + dyn * dyn);
+      }
+
+      const isMediate = i > 0 && i < last;
+      const r = isMediate ? roundness : 0;
+      const ratioNext = r / distanceNext;
+      const ratioPrev = r / distancePrev;
+
+      const njp: Point = isMediate
+        ? [p[0] + dxn * ratioNext, p[1] + dyn * ratioNext]
+        : p;
+      const pjp: Point = isMediate
+        ? [p[0] + dxp * ratioPrev, p[1] + dyp * ratioPrev]
+        : p;
+
+      if (i > 0) {
+        parts.push(`L ${pjp[0]} ${pjp[1]}`);
+      }
+
+      if (isMediate) {
+        parts.push(`C ${p[0]} ${p[1]} ${p[0]} ${p[1]} ${njp[0]} ${njp[1]}`);
+      }
+    });
+  }
+
+  return parts.join(" ");
 };
