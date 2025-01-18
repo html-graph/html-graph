a83cb913289d95feb17773f18b19aa958dbf28a8
Dmitry Marov <d.marov94@gmail.com>
2025-01-18 17:54:23 +0300

    wip

--- use-cases/62-virtual-scroll/main.ts
+++ use-cases/62-virtual-scroll/main.ts
@@ -31,8 +31,8 @@ let prevPortId: string | null = null;
 
 for (let i = 0; i < 20; i++) {
   for (let j = 0; j < 20; j++) {
-    const frontPortId = `port-${id}-in`;
-    const backPortId = `port-${id}-out`;
+    const frontPortId = `in-${id}`;
+    const backPortId = `out-${id}`;
 
     canvas.addNode(
       createBasicNode({
