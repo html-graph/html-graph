860551b6ad78ad2d6f30f9610c3e3a284e69b51d
Dmitry Marov <d.marov94@gmail.com>
2025-01-25 14:52:32 +0300

    added eslint ignore dir

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["dist/**/*"],
+    ignores: ["dist/**/*", "dist-docs/**/*"],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
