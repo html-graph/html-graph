1f4c2df040e82fed1cad654d3f4dfa8cb703063f
Dmitry Marov <d.marov94@gmail.com>
2025-01-25 19:34:20 +0300

    added unit tests for bezier edge

---
+++ lib/edges/bezier/bezier-edge-shape.spec.ts
@@ -0,0 +1,120 @@
+import { EdgeShape } from "../edge-shape";
+import { BezierEdgeShape } from "./bezier-edge-shape";
+
+const createBezierEdge = (
+  hasSourceArrow: boolean,
+  hasTargetArrow: boolean,
+): EdgeShape => {
+  const color: string = "#FFFFFF";
+  const width: number = 2;
+  const curvature: number = 40;
+  const arrowLength: number = 10;
+  const arrowWidth: number = 3;
+
+  return new BezierEdgeShape(
+    color,
+    width,
+    curvature,
+    arrowLength,
+    arrowWidth,
+    hasSourceArrow,
+    hasTargetArrow,
+  );
+};
+
+describe("BezierEdgeShape", () => {
+  it("should have only line element", () => {
+    const shape = createBezierEdge(false, false);
+
+    const childrenCount = shape.svg.children[0].children.length;
+
+    expect(childrenCount).toBe(1);
+  });
+
+  it("should have line and arrow element", () => {
+    const shape = createBezierEdge(true, false);
+
+    const childrenCount = shape.svg.children[0].children.length;
+
+    expect(childrenCount).toBe(2);
+  });
+
+  it("should have line and 2 arrows element", () => {
+    const shape = createBezierEdge(true, true);
+
+    const childrenCount = shape.svg.children[0].children.length;
+
+    expect(childrenCount).toBe(3);
+  });
+
+  it("should apply specified mirroring to group", () => {
+    const shape = createBezierEdge(false, false);
+
+    shape.update({ x: 100, y: 100 }, 1, -1, 0, 0);
+
+    const g = shape.svg.children[0] as SVGGElement;
+
+    expect(g.style.transform).toBe("scale(1, -1)");
+  });
+
+  it("should create line path without arrows", () => {
+    const shape = createBezierEdge(false, false);
+
+    shape.update({ x: 100, y: 100 }, 1, 1, 0, 0);
+
+    const g = shape.svg.children[0];
+    const line = g.children[0];
+
+    expect(line.getAttribute("d")).toBe(
+      "M 0 0 L 10 0 M 10 0 C 50 0, 50 100, 90 100 M 90 100 L 100 100",
+    );
+  });
+
+  it("should create line path accounting for target arrow", () => {
+    const shape = createBezierEdge(false, true);
+
+    shape.update({ x: 100, y: 100 }, 1, 1, 0, 0);
+
+    const g = shape.svg.children[0];
+    const line = g.children[0];
+
+    expect(line.getAttribute("d")).toBe(
+      "M 0 0 L 10 0 M 10 0 C 50 0, 50 100, 90 100",
+    );
+  });
+
+  it("should create line path accounting for source arrow", () => {
+    const shape = createBezierEdge(true, false);
+
+    shape.update({ x: 100, y: 100 }, 1, 1, 0, 0);
+
+    const g = shape.svg.children[0];
+    const line = g.children[0];
+
+    expect(line.getAttribute("d")).toBe(
+      "M 10 0 C 50 0, 50 100, 90 100 M 90 100 L 100 100",
+    );
+  });
+
+  it("should create path for target arrow", () => {
+    const shape = createBezierEdge(false, true);
+
+    shape.update({ x: 100, y: 100 }, 1, 1, 0, 0);
+
+    const g = shape.svg.children[0];
+    const arrow = g.children[1];
+
+    expect(arrow.getAttribute("d")).toBe("M 100 100 L 90 103 L 90 97");
+  });
+
+  it("should create path for source arrow", () => {
+    const shape = createBezierEdge(true, false);
+
+    shape.update({ x: 100, y: 100 }, 1, 1, 0, 0);
+
+    const g = shape.svg.children[0];
+    const arrow = g.children[1];
+
+    expect(arrow.getAttribute("d")).toBe("M 0 0 L 10 3 L 10 -3");
+  });
+});
