7e0be60fdaeab913b7e6b7fa0dc4a8b5df4dd97c
Dmitry Marov <d.marov94@gmail.com>
2025-01-25 13:00:12 +0300

    added edge utils unit tests

---
+++ lib/edges/utils/create-arrow-path/create-arrow-path.spec.ts
@@ -0,0 +1,58 @@
+import { Point } from "@/point";
+import { createArrowPath } from "./create-arrow-path";
+
+describe("createArrowPath", () => {
+  it("should create arrow path", () => {
+    const vector: Point = { x: 1, y: 0 };
+    const shiftX = 0;
+    const shiftY = 0;
+    const arrowLength = 10;
+    const arrowWidth = 5;
+
+    const path = createArrowPath(
+      vector,
+      shiftX,
+      shiftY,
+      arrowLength,
+      arrowWidth,
+    );
+
+    expect(path).toBe("M 0 0 L 10 5 L 10 -5");
+  });
+
+  it("should create rotated arrow path", () => {
+    const vector: Point = { x: 0, y: 1 };
+    const shiftX = 0;
+    const shiftY = 0;
+    const arrowLength = 10;
+    const arrowWidth = 5;
+
+    const path = createArrowPath(
+      vector,
+      shiftX,
+      shiftY,
+      arrowLength,
+      arrowWidth,
+    );
+
+    expect(path).toBe("M 0 0 L -5 10 L 5 10");
+  });
+
+  it("should create shifted arrow path", () => {
+    const vector: Point = { x: 1, y: 0 };
+    const shiftX = 5;
+    const shiftY = 10;
+    const arrowLength = 10;
+    const arrowWidth = 5;
+
+    const path = createArrowPath(
+      vector,
+      shiftX,
+      shiftY,
+      arrowLength,
+      arrowWidth,
+    );
+
+    expect(path).toBe("M 5 10 L 15 15 L 15 5");
+  });
+});
