601ca7460df38d1dcd886f218acdbfca28e22c44
Dmitry Marov <d.marov94@gmail.com>
2025-02-05 19:29:36 +0300

    refactored use cases

--- use-cases/001-getting-started/main.ts
+++ use-cases/001-getting-started/main.ts
@@ -4,42 +4,43 @@ import {
   HtmlGraphBuilder,
 } from "@html-graph/html-graph";
 
-const canvas = new HtmlGraphBuilder().build();
+const canvas = new HtmlGraphBuilder()
+  .setOptions({
+    nodes: {
+      priority: 1,
+    },
+    edges: {
+      priority: 0,
+      shape: {
+        type: "bezier",
+        arrowLength: 25,
+      },
+    },
+  })
+  .build();
 
-const node1 = document.createElement("div");
-node1.classList.add("node");
-node1.innerText = "1";
+const createNode = (params: {
+  portId: string;
+  x: number;
+  y: number;
+}): AddNodeRequest => {
+  const node = document.createElement("div");
+  node.classList.add("node");
 
-const node2 = document.createElement("div");
-node2.classList.add("node");
-node2.innerText = "2";
+  const port = document.createElement("div");
+  port.classList.add("port");
 
-const port1 = document.createElement("div");
-port1.classList.add("port");
-port1.style.right = "0";
+  node.appendChild(port);
 
-const port2 = document.createElement("div");
-port2.classList.add("port");
-port2.style.left = "0";
-
-node1.appendChild(port1);
-node2.appendChild(port2);
+  return {
+    element: node,
+    x: params.x,
+    y: params.y,
+    ports: [{ id: params.portId, element: port }],
+  };
+};
 
 const canvasElement = document.getElementById("canvas")!;
-
-const addNode1Request: AddNodeRequest = {
-  element: node1,
-  x: 200,
-  y: 300,
-  ports: [{ id: "port-1", element: port1 }],
-};
-
-const addNode2Request: AddNodeRequest = {
-  element: node2,
-  x: 600,
-  y: 500,
-  ports: [{ id: "port-2", element: port2 }],
-};
 
 const addEdgeRequest: AddEdgeRequest = {
   from: "port-1",
@@ -48,6 +49,6 @@ const addEdgeRequest: AddEdgeRequest = {
 
 canvas
   .attach(canvasElement)
-  .addNode(addNode1Request)
-  .addNode(addNode2Request)
+  .addNode(createNode({ portId: "port-1", x: 100, y: 100 }))
+  .addNode(createNode({ portId: "port-2", x: 500, y: 500 }))
   .addEdge(addEdgeRequest);
