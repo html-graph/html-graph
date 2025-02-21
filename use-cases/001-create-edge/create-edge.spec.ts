e320f70e48592603e1c2d6fa977766d92a05c5fc
Dmitry Marov <d.marov94@gmail.com>
2025-02-21 18:46:28 +0300

    e2e wip

--- use-cases/001-create-edge/create-edge.spec.ts
+++ use-cases/001-create-edge/create-edge.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
+import { e2eBase } from "../shared/e2e-base";
 
 test("should create nodes and edges", async ({ page }) => {
-  await page.goto("http://localhost:3100/use-cases/001-create-edge/");
-  await expect(page).toHaveScreenshot();
+  await page.goto(`${e2eBase}/001-create-edge/`);
+  await expect(page).toHaveScreenshot("create-edge.png");
 });
