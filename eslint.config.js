313ebc15b0bbc8625c016b9bd6ddce89ead469f9
Dmitry Marov <d.marov94@gmail.com>
2025-02-22 16:13:34 +0300

    fixed lint

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,12 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["dist/**/*", "dist-docs/**/*"],
+    ignores: [
+      "dist/**/*",
+      "dist-docs/**/*",
+      "playwright-report/**/*",
+      "coverage/**/*",
+    ],
   },
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
