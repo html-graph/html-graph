ca86ca9fc862da42264193f50d718e030a828fc2
Dmitry Marov <d.marov94@gmail.com>
2025-03-04 21:53:17 +0300

    removed docs

--- TODO.md
+++ TODO.md
@@ -3,11 +3,11 @@
 - virtual scroll
 - layouts
 - drag edge creation
+- background decorator
 - arrows customization
 - sankey connections
 - transformable canvas shift limit when scaling ?
 - optimize memory draggable nodes
-- optimize background drawing
 - node drag handle example
 - snap nodes to grid
 - node creation factory function?
--- lib/canvas/user-draggable-nodes-canvas/create-options/drag-options.ts
+++ lib/canvas/user-draggable-nodes-canvas/create-options/drag-options.ts
@@ -10,6 +10,6 @@ export interface DragOptions {
   readonly events?: {
     readonly onNodeDrag?: (payload: NodeDragPayload) => void;
     readonly onBeforeNodeDrag?: (payload: NodeDragPayload) => boolean;
-    readonly onNodeDragFinished?: (nodeId: NodeDragPayload) => void;
+    readonly onNodeDragFinished?: (payload: NodeDragPayload) => void;
   };
 }
