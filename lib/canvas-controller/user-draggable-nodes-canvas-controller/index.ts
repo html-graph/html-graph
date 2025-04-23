d5872ec7face2fae96a836cde4aad368e1beff55
Dmitry Marov <d.marov94@gmail.com>
2025-04-23 20:00:20 +0300

    removed unused files

--- lib/canvas-controller/user-draggable-nodes-canvas-controller/index.ts
+++
@@ -1,3 +0,0 @@
-export { UserDraggableNodesCanvasController } from "./user-draggable-nodes-canvas-controller";
-export type { DragOptions } from "./create-options";
-export type { NodeDragPayload } from "./node-drag-payload";
--- lib/canvas-controller/user-draggable-nodes-canvas-controller/node-drag-payload.ts
+++
@@ -1,6 +0,0 @@
-export interface NodeDragPayload {
-  readonly nodeId: unknown;
-  readonly element: HTMLElement;
-  readonly x: number;
-  readonly y: number;
-}
