becf0c5739d117348630db7248c2df76498635d0
Dmitry Marov <d.marov94@gmail.com>
2025-05-02 14:49:35 +0300

    fixed e2e

--- use-cases/061-custom-canvas-shift-cursor/e2e.spec.ts
+++ use-cases/061-custom-canvas-shift-cursor/e2e.spec.ts
@@ -7,7 +7,7 @@ test("should set custom canvas shift cursor", async ({ page }) => {
   await page.mouse.move(700, 300);
   await page.mouse.down();
 
-  const canvas = page.locator("#canvas");
+  const canvas = page.locator('//*[@id="canvas"]/div[1]/div[2]');
   const cursor = await canvas.evaluate((element) => element.style.cursor);
 
   expect(cursor).toBe("crosshair");
