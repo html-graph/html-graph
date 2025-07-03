34e1743a48e8727c2f7a3d7c5dc71cf784d2af87
Dmitry Marov <d.marov94@gmail.com>
2025-07-03 19:51:32 +0300

    refactored

--- src/html-view/core-html-view/utils/index.ts
+++ src/html-view/core-html-view/utils/index.ts
@@ -1,3 +1,5 @@
 export { createContainer } from "./create-container";
 export { createNodeWrapper } from "./create-node-wrapper";
 export { createHost } from "./create-host";
+export { prepareNodeElement } from "./prepare-node-element";
+export { resetNodeElement } from "./reset-node-element";
