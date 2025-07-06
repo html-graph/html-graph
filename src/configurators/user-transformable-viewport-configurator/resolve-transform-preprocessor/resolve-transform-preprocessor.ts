857b7a0aa7a41721c2538cd106afe5a3659bd149
Dmitry Marov <d.marov94@gmail.com>
2025-07-06 14:03:44 +0300

    fixed cycle dependency

--- src/configurators/user-transformable-viewport-configurator/resolve-transform-preprocessor/resolve-transform-preprocessor.ts
+++ src/configurators/user-transformable-viewport-configurator/resolve-transform-preprocessor/resolve-transform-preprocessor.ts
@@ -1,4 +1,4 @@
-import { TransformPreprocessorConfig } from "../config";
+import { TransformPreprocessorConfig } from "../config/transform-preprocessor-config";
 import {
   createScaleLimitTransformPreprocessor,
   createShiftLimitTransformPreprocessor,
