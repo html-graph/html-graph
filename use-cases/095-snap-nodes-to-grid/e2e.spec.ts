52d10f561b63f50ebedcc575abc8cafcf8dd4deb
Dmitry Marov <d.marov94@gmail.com>
2025-07-27 16:18:46 +0300

    added e2e

---
+++ use-cases/095-snap-nodes-to-grid/e2e.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+import { e2eBase } from "../shared/e2e-base";
+
+test("should have draggable nodes", async ({ page }) => {
+  await page.goto(`${e2eBase}/095-snap-nodes-to-grid/`);
+  await expect(page).toHaveScreenshot("initial.png");
+
+  await page.mouse.move(200, 400);
+  await page.mouse.down();
+  await page.mouse.move(488, 400);
+
+  await expect(page).toHaveScreenshot("after-move.png");
+});
