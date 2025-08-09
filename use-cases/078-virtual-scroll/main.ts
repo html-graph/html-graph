6e760c0cb37a0e61b1b81b2e881870aa869cb8c9
Dmitry Marov <d.marov94@gmail.com>
2025-08-09 13:09:55 +0300

    improved virtual scroll example

--- use-cases/078-virtual-scroll/main.ts
+++ use-cases/078-virtual-scroll/main.ts
@@ -1,4 +1,5 @@
 import {
+  AddNodeRequest,
   Canvas,
   CanvasBuilder,
   CanvasDefaults,
@@ -6,6 +7,7 @@ import {
   VirtualScrollConfig,
 } from "@html-graph/html-graph";
 import { createInOutNode } from "../shared/create-in-out-node";
+import { executeMacrotask } from "../shared/execute-macrotask";
 
 const canvasElement: HTMLElement = document.getElementById("canvas")!;
 
@@ -36,32 +38,72 @@ const canvas: Canvas = new CanvasBuilder(canvasElement)
   .setDefaults(defaults)
   .enableUserTransformableViewport(transformConfig)
   .enableVirtualScroll(virtualScrollConfig)
+  .enableBackground()
   .build();
 
-let cnt = 0;
+document.getElementById("start")!.addEventListener("click", async () => {
+  document.getElementById("start")!.style.display = "none";
+  document.getElementById("spinner")!.style.display = "block";
 
-let prevPortId: unknown | null = null;
+  await initiateGraph();
 
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < 10000; j++) {
-    const frontPortId = `node-${cnt}-in`;
-    const backPortId = `node-${cnt}-out`;
+  document.getElementById("loader")!.style.display = "none";
+});
 
-    canvas.addNode(
-      createInOutNode({
-        name: `Node ${cnt}`,
-        x: j * 300,
-        y: i * 300,
-        frontPortId,
-        backPortId,
-      }),
-    );
+const initiateGraph = async (): Promise<void> => {
+  let prevPortId: unknown | null = null;
 
-    if (prevPortId !== null) {
-      canvas.addEdge({ from: prevPortId, to: frontPortId });
-    }
+  const total = 100_000;
+  const layer = total / 10;
+  const collection = Array.from({ length: total }).map((_, i) => i);
 
-    prevPortId = backPortId;
-    cnt++;
+  const iterator = collection.values();
+  let current = iterator.next();
+  let processedTotal = 0;
+
+  while (!current.done) {
+    await executeMacrotask(() => {
+      let processed = 0;
+
+      while (!current.done && processed < 1000) {
+        const iter = current.value;
+        const frontPortId = `node-${iter}-in`;
+        const backPortId = `node-${iter}-out`;
+
+        const i = iter % layer;
+        const j = Math.floor(iter / layer);
+
+        const request: AddNodeRequest = createInOutNode({
+          name: `Node ${iter}`,
+          x: i * 300,
+          y: j * 300,
+          frontPortId,
+          backPortId,
+        });
+
+        canvas.addNode(request);
+
+        if (prevPortId !== null) {
+          const nodeFromId = canvas.graph.getPort(prevPortId)!.nodeId;
+          const nodeFrom = canvas.graph.getNode(nodeFromId)!;
+          const nodeToId = canvas.graph.getPort(frontPortId)!.nodeId;
+          const nodeTo = canvas.graph.getNode(nodeToId)!;
+
+          if (nodeFrom.y === nodeTo.y) {
+            canvas.addEdge({ from: prevPortId, to: frontPortId });
+          }
+        }
+
+        prevPortId = backPortId;
+        current = iterator.next();
+        processed++;
+      }
+
+      processedTotal += processed;
+
+      const percent = (processedTotal / total) * 100;
+
+      document.getElementById("status")!.innerText = `${Math.floor(percent)}%`;
+    });
   }
-}
+};
