2509c37a1c0b7eb8e27def05152207792b82340d
Dmitry Marov <d.marov94@gmail.com>
2025-08-10 15:22:55 +0300

    wip

--- src/edges/arrow-renderer/resolve-arrow-renderer/resolve-arrow-renderer.spec.ts
+++ src/edges/arrow-renderer/resolve-arrow-renderer/resolve-arrow-renderer.spec.ts
@@ -3,18 +3,28 @@ import { resolveArrowRenderer } from "./resolve-arrow-renderer";
 import { ArrowRenderer } from "../arrow-renderer";
 
 describe("resolveArrowRenderer", () => {
-  it("should resolve polygon arrow renderer by default", () => {
+  it("should resolve circle arrow renderer by default", () => {
     const renderer = resolveArrowRenderer({});
     const direction: Point = { x: 1, y: 0 };
     const shift: Point = { x: 0, y: 0 };
 
     expect(renderer({ direction, shift, arrowLength: 15 })).toBe(
-      "M 0 0 L 15 4 L 15 -4 Z",
+      "M 0 0 A 29.0625 29.0625 0 0 0 18.035413153457 -6.27318718381113 A 8 8 0 0 0 18.035413153457 6.27318718381113 A 29.0625 29.0625 0 0 0 0 0",
+    );
+  });
+
+  it("should resolve circle arrow renderer with specified radius", () => {
+    const renderer = resolveArrowRenderer({ type: "circle", radius: 15 });
+    const direction: Point = { x: 1, y: 0 };
+    const shift: Point = { x: 0, y: 0 };
+
+    expect(renderer({ direction, shift, arrowLength: 15 })).toBe(
+      "M 0 0 A 22.5 22.5 0 0 0 18 -9 A 15 15 0 0 0 18 9 A 22.5 22.5 0 0 0 0 0",
     );
   });
 
   it("should resolve polygon arrow renderer with specified radius", () => {
-    const renderer = resolveArrowRenderer({ radius: 10 });
+    const renderer = resolveArrowRenderer({ type: "polygon", radius: 10 });
     const direction: Point = { x: 1, y: 0 };
     const shift: Point = { x: 0, y: 0 };
 
