5200f9581e0f89aaebc3f3077a1f58b18168e058
Dmitry Marov <d.marov94@gmail.com>
2025-08-25 17:52:15 +0300

    added unit tests

---
+++ src/configurators/layout-configurator/layout-configurator.spec.ts
@@ -0,0 +1,97 @@
+import { Canvas } from "@/canvas";
+import { GraphStore } from "@/graph-store";
+import { ViewportStore } from "@/viewport-store";
+import { Graph } from "@/graph";
+import { Viewport } from "@/viewport";
+import { CoreHtmlView, HtmlView, LayoutHtmlView } from "@/html-view";
+import { defaultCanvasParams, wait } from "@/mocks";
+import { LayoutConfig } from "./layout-config";
+import { DummyLayoutAlgorithm } from "@/mocks/dummy-layout-algorithm.mock";
+import { LayoutConfigurator } from "./layout-configurator";
+import { EventSubject } from "@/event-subject";
+
+const createCanvas = (): Canvas => {
+  const graphStore = new GraphStore();
+  const viewportStore = new ViewportStore();
+  const graph = new Graph(graphStore);
+  const viewport = new Viewport(viewportStore);
+  const element = document.createElement("div");
+  let htmlView: HtmlView = new CoreHtmlView(graphStore, viewportStore, element);
+  htmlView = new LayoutHtmlView(htmlView, graphStore);
+
+  const canvas = new Canvas(
+    graph,
+    viewport,
+    graphStore,
+    viewportStore,
+    htmlView,
+    defaultCanvasParams,
+  );
+
+  return canvas;
+};
+
+describe("LayoutConfigurator", () => {
+  it("should configure topology change layout application strategy", async () => {
+    const canvas = createCanvas();
+    const config: LayoutConfig = {
+      algorithm: new DummyLayoutAlgorithm(),
+      applicationStrategy: { type: "topologyChange" },
+    };
+
+    LayoutConfigurator.configure(canvas, config);
+
+    canvas.addNode({ id: "node-1", element: document.createElement("div") });
+
+    await wait(0);
+
+    const { x, y } = canvas.graph.getNode("node-1")!;
+    expect({ x, y }).toEqual({ x: 0, y: 0 });
+  });
+
+  it("should configure manual layout application strategy", () => {
+    const canvas = createCanvas();
+    const trigger = new EventSubject<void>();
+    const config: LayoutConfig = {
+      algorithm: new DummyLayoutAlgorithm(),
+      applicationStrategy: { type: "manual", trigger },
+    };
+
+    LayoutConfigurator.configure(canvas, config);
+
+    canvas.addNode({ id: "node-1", element: document.createElement("div") });
+
+    trigger.emit();
+
+    const { x, y } = canvas.graph.getNode("node-1")!;
+
+    expect({ x, y }).toEqual({ x: 0, y: 0 });
+  });
+
+  it("should apply specified transformation", () => {
+    const canvas = createCanvas();
+    const trigger = new EventSubject<void>();
+    const config: LayoutConfig = {
+      algorithm: new DummyLayoutAlgorithm(),
+      applicationStrategy: { type: "manual", trigger },
+      transform: {
+        a: 1,
+        b: 0,
+        c: 100,
+        d: 0,
+        e: 1,
+        f: 100,
+      },
+    };
+
+    LayoutConfigurator.configure(canvas, config);
+
+    canvas.addNode({ id: "node-1", element: document.createElement("div") });
+
+    trigger.emit();
+
+    const { x, y } = canvas.graph.getNode("node-1")!;
+
+    expect({ x, y }).toEqual({ x: 100, y: 100 });
+  });
+});
---
+++ src/mocks/dummy-layout-algorithm.mock.ts
@@ -0,0 +1,16 @@
+import { Graph } from "@/graph";
+import { Identifier } from "@/identifier";
+import { LayoutAlgorithm } from "@/layout-algorithm";
+import { Point } from "@/point";
+
+export class DummyLayoutAlgorithm implements LayoutAlgorithm {
+  public calculateCoordinates(graph: Graph): ReadonlyMap<Identifier, Point> {
+    const result = new Map<Identifier, Point>();
+
+    graph.getAllNodeIds().forEach((nodeId) => {
+      result.set(nodeId, { x: 0, y: 0 });
+    });
+
+    return result;
+  }
+}
