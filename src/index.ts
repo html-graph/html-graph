6e39f8bd052045923aa700217c457ad2edd4adb7
Dmitry Marov <d.marov94@gmail.com>
2025-08-25 19:10:32 +0300

    refactored

--- src/index.ts
+++ src/index.ts
@@ -78,4 +78,3 @@ export type { Identifier } from "./identifier";
 export type { LayoutAlgorithm, TransformationMatrix } from "./layout-algorithm";
 
 export { EventSubject } from "./event-subject";
-export type { EventEmitter, EventHandler } from "./event-subject";
