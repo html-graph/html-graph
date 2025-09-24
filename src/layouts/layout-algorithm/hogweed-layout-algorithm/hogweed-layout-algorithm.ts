2b8087aa7bf42750fa7bbfb55bf3d3846328eecc
Dmitry Marov <d.marov94@gmail.com>
2025-09-24 19:03:17 +0300

    wip

--- src/layouts/layout-algorithm/hogweed-layout-algorithm/hogweed-layout-algorithm.ts
+++ src/layouts/layout-algorithm/hogweed-layout-algorithm/hogweed-layout-algorithm.ts
@@ -6,6 +6,9 @@ import { LayoutAlgorithm } from "../layout-algorithm";
 export class HogweedLayoutAlgorithm implements LayoutAlgorithm {
   public calculateCoordinates(graph: Graph): ReadonlyMap<Identifier, Point> {
     console.log(graph);
+
+    // sort nodes by most adjacent connections
+    // formate clicks
     throw new Error("Method not implemented.");
   }
 }
