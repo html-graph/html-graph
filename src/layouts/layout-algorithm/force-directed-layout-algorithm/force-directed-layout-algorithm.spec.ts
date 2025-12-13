85b98de5afa232744ef1ca26499756dc3a1c9f16
Dmitry Marov <d.marov94@gmail.com>
2025-12-13 14:35:39 +0300

    wip

--- src/layouts/layout-algorithm/force-directed-layout-algorithm/force-directed-layout-algorithm.spec.ts
+++ src/layouts/layout-algorithm/force-directed-layout-algorithm/force-directed-layout-algorithm.spec.ts
@@ -1,47 +1,56 @@
 import { createCanvas } from "@/mocks";
 import { ForceDirectedLayoutAlgorithm } from "./force-directed-layout-algorithm";
+import { LayoutAlgorithm } from "../layout-algorithm";
+import { Canvas } from "@/canvas";
+
+const initCanvas = (canvas: Canvas): void => {
+  canvas.addNode({
+    id: "node-1",
+    element: document.createElement("div"),
+    x: 10,
+    y: 0,
+    ports: [
+      {
+        id: "port-1",
+        element: document.createElement("div"),
+      },
+    ],
+  });
+
+  canvas.addNode({
+    id: "node-2",
+    element: document.createElement("div"),
+    x: 20,
+    y: 0,
+    ports: [
+      {
+        id: "port-2",
+        element: document.createElement("div"),
+      },
+    ],
+  });
+};
+
+const createAlgorithm = (): LayoutAlgorithm => {
+  return new ForceDirectedLayoutAlgorithm({
+    dtSec: 1,
+    maxIterations: 1,
+    rand: (): number => 0,
+    maxTimeDeltaSec: 1,
+    nodeCharge: 10,
+    nodeMass: 1,
+    edgeEquilibriumLength: 8,
+    effectiveDistance: 1000,
+    edgeStiffness: 1,
+  });
+};
 
 describe("ForceDirectedLayoutAlgorithm", () => {
   it("should calculate coordinates based on total forces", () => {
     const canvas = createCanvas();
+    initCanvas(canvas);
 
-    canvas.addNode({
-      id: "node-1",
-      element: document.createElement("div"),
-      x: 10,
-      y: 0,
-      ports: [
-        {
-          id: "port-1",
-          element: document.createElement("div"),
-        },
-      ],
-    });
-
-    canvas.addNode({
-      id: "node-2",
-      element: document.createElement("div"),
-      x: 20,
-      y: 0,
-      ports: [
-        {
-          id: "port-2",
-          element: document.createElement("div"),
-        },
-      ],
-    });
-
-    const algorithm = new ForceDirectedLayoutAlgorithm({
-      dtSec: 1,
-      maxIterations: 1,
-      rand: (): number => 0,
-      maxTimeDeltaSec: 1,
-      nodeCharge: 10,
-      nodeMass: 1,
-      edgeEquilibriumLength: 8,
-      effectiveDistance: 1000,
-      edgeStiffness: 1,
-    });
+    const algorithm = createAlgorithm();
 
     const nextCoords = algorithm.calculateCoordinates(canvas.graph);
 
