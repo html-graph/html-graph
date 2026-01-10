df6ec3e04112aac9001d6f69e34ba1b836409b37
Dmitry Marov <d.marov94@gmail.com>
2026-01-10 15:15:01 +0300

    wip

--- src/layouts/layout-algorithm/index.ts
+++ src/layouts/layout-algorithm/index.ts
@@ -1,3 +1,4 @@
 export type { LayoutAlgorithm } from "./layout-algorithm";
 export { ForceDirectedLayoutAlgorithm } from "./force-directed-layout-algorithm";
 export { RandomFillerLayoutAlgorithm } from "./random-filler-layout-algorithm";
+export type { RandomFillerLayoutAlgorithmParams } from "./random-filler-layout-algorithm";
