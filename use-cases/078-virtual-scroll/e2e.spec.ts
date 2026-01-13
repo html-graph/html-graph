9ead8961dce1782642bfb12848b61f79ec541f13
Dmitry Marov <d.marov94@gmail.com>
2026-01-13 10:41:34 +0300

    wip

--- use-cases/078-virtual-scroll/e2e.spec.ts
+++ use-cases/078-virtual-scroll/e2e.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import { e2eBase } from "../shared/e2e-base";
 
 test("should display canvas with virtual scroll", async ({ page }) => {
-  test.setTimeout(10_000);
+  test.setTimeout(60_000);
   await page.goto(`${e2eBase}/078-virtual-scroll/`);
 
   await page.getByTestId("start").click();
