ce02cf686f7d3313f3e9692b8b23f5a1a53c6edd
Dmitry Marov <d.marov94@gmail.com>
2026-01-18 11:21:10 +0300

    refactored

--- src/configurators/shared/layout-applier/layout-applier.spec.ts
+++ src/configurators/shared/layout-applier/layout-applier.spec.ts
@@ -1,16 +1,19 @@
 import { createCanvas, DummyLayoutAlgorithm } from "@/mocks";
 import { LayoutApplier } from "./layout-applier";
 import { Identifier } from "@/identifier";
+import { LayoutApplierParams } from "./layout-applier-params";
 
 describe("LayoutApplier", () => {
   it("should apply specified layout", () => {
     const canvas = createCanvas();
     const layoutAlgorithm = new DummyLayoutAlgorithm();
-    const applier = new LayoutApplier(canvas, layoutAlgorithm, {
+    const params: LayoutApplierParams = {
       staticNodeResolver: (): boolean => false,
       onBeforeApplied: (): void => {},
       onAfterApplied: (): void => {},
-    });
+    };
+
+    const applier = new LayoutApplier(canvas, layoutAlgorithm, params);
 
     canvas.addNode({
       id: "node-1",
@@ -25,11 +28,13 @@ describe("LayoutApplier", () => {
   it("should not apply layout for static nodes", () => {
     const canvas = createCanvas();
     const layoutAlgorithm = new DummyLayoutAlgorithm();
-    const applier = new LayoutApplier(canvas, layoutAlgorithm, {
+    const params: LayoutApplierParams = {
       staticNodeResolver: (nodeId: Identifier): boolean => nodeId === "node-1",
       onBeforeApplied: (): void => {},
       onAfterApplied: (): void => {},
-    });
+    };
+
+    const applier = new LayoutApplier(canvas, layoutAlgorithm, params);
 
     canvas.addNode({
       id: "node-1",
@@ -45,11 +50,13 @@ describe("LayoutApplier", () => {
     const canvas = createCanvas();
     const layoutAlgorithm = new DummyLayoutAlgorithm();
     const onBeforeApplied = jest.fn();
-    const applier = new LayoutApplier(canvas, layoutAlgorithm, {
+    const params: LayoutApplierParams = {
       staticNodeResolver: (): boolean => false,
       onBeforeApplied,
       onAfterApplied: (): void => {},
-    });
+    };
+
+    const applier = new LayoutApplier(canvas, layoutAlgorithm, params);
 
     applier.apply();
 
@@ -60,11 +67,13 @@ describe("LayoutApplier", () => {
     const canvas = createCanvas();
     const layoutAlgorithm = new DummyLayoutAlgorithm();
     const onAfterApplied = jest.fn();
-    const applier = new LayoutApplier(canvas, layoutAlgorithm, {
+    const params: LayoutApplierParams = {
       staticNodeResolver: (): boolean => false,
       onBeforeApplied: (): void => {},
       onAfterApplied,
-    });
+    };
+
+    const applier = new LayoutApplier(canvas, layoutAlgorithm, params);
 
     applier.apply();
 
