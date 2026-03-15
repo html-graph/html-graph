f244a6820eadcb4af5d250286ade7739de280d7e
Dmitry Marov <d.marov94@gmail.com>
2026-03-15 16:53:22 +0300

    wip

--- use-cases/117-reverse-horizontal-edges/main.ts
+++ use-cases/117-reverse-horizontal-edges/main.ts
@@ -27,7 +27,7 @@ const canvas: Canvas = builder
 
 const addNode1Request: AddNodeRequest = createInOutNode({
   name: "Node 1",
-  x: 200,
+  x: 500,
   y: 400,
   frontPortId: "node-1-in",
   backPortId: "node-1-out",
@@ -35,7 +35,7 @@ const addNode1Request: AddNodeRequest = createInOutNode({
 
 const addNode2Request: AddNodeRequest = createInOutNode({
   name: "Node 2",
-  x: 500,
+  x: 200,
   y: 500,
   frontPortId: "node-2-in",
   backPortId: "node-2-out",
@@ -43,7 +43,7 @@ const addNode2Request: AddNodeRequest = createInOutNode({
 
 const addNode3Request: AddNodeRequest = createInOutNode({
   name: "Node 3",
-  x: 200,
+  x: 500,
   y: 650,
   frontPortId: "node-3-in",
   backPortId: "node-3-out",
