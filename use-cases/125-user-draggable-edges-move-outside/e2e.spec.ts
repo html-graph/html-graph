bf330aa99a626113856b111c8c498c85594d57e8
Dmitry Marov <d.marov94@gmail.com>
2026-04-08 20:29:29 +0300

    wip

---
+++ use-cases/125-user-draggable-edges-move-outside/e2e.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from "@playwright/test";
+import { e2eBase } from "../shared/e2e-base";
+
+test("should apply draggable edges", async ({ page }) => {
+  await page.goto(`${e2eBase}/125-user-draggable-edges-move-outside/`);
+  await expect(page).toHaveScreenshot("initial.png");
+
+  await page.mouse.move(450, 500);
+  await page.mouse.down();
+  await page.mouse.move(-100, -100);
+
+  await expect(page).toHaveScreenshot("after-pointer outside.png");
+});
