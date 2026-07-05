98e4fb9635b1d8b1e3c51b2a79807b941df70069
Dmitry Marov <d.marov94@gmail.com>
2026-07-05 15:12:02 +0300

    wip

--- src/edges/paths/shared/shared/transpose-point/transpose-point.spec.ts
+++ src/edges/paths/shared/shared/transpose-point/transpose-point.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { transposePoint } from "./transpose-point";
 
-describe("transpose", () => {
+describe("transposePoint", () => {
   it("should transpose point", () => {
     expect(transposePoint({ x: 1, y: 2 })).toEqual({ x: 2, y: 1 });
   });
