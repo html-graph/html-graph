62ca38967b516d5b890abe8590c841e205e0ad2a
Dmitry Marov <d.marov94@gmail.com>
2026-09-20 12:48:58 +0300

    wip

--- src/edge-shapes/orthogonal-edge-shape/orthogonal-edge-shape.ts
+++ src/edge-shapes/orthogonal-edge-shape/orthogonal-edge-shape.ts
@@ -39,10 +39,6 @@ export class OrthogonalEdgeShape implements StructuredEdgeShape {
 
   private readonly detourDistance: number;
 
-  private readonly hasSourceArrow: boolean;
-
-  private readonly hasTargetArrow: boolean;
-
   private readonly pathShape: PathEdgeShape;
 
   private readonly createPortCyclePath: EdgePathFactory = (
@@ -98,18 +94,14 @@ export class OrthogonalEdgeShape implements StructuredEdgeShape {
 
     this.detourDistance =
       params?.detourDistance ?? edgeConstants.detourDistance;
-    this.hasSourceArrow =
-      params?.hasSourceArrow ?? edgeConstants.hasSourceArrow;
-    this.hasTargetArrow =
-      params?.hasTargetArrow ?? edgeConstants.hasTargetArrow;
 
     this.pathShape = new PathEdgeShape({
       color: params?.color ?? edgeConstants.color,
       width: params?.width ?? edgeConstants.width,
       arrowRenderer: resolveArrowRenderer(params?.arrowRenderer ?? {}),
       arrowLength: this.arrowLength,
-      hasSourceArrow: this.hasSourceArrow,
-      hasTargetArrow: this.hasTargetArrow,
+      hasSourceArrow: params?.hasSourceArrow ?? edgeConstants.hasSourceArrow,
+      hasTargetArrow: params?.hasTargetArrow ?? edgeConstants.hasTargetArrow,
       createPortCyclePath: this.createPortCyclePath,
       createNodeCyclePath: this.createNodeCyclePath,
       createLinePath: this.createLinePath,
