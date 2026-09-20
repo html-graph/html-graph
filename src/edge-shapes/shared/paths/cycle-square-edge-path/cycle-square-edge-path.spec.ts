c8febc4762b3e330f29beba91be66c6b772bcf59
Dmitry Marov <d.marov94@gmail.com>
2026-09-20 12:44:41 +0300

    wip

--- src/edge-shapes/shared/paths/cycle-square-edge-path/cycle-square-edge-path.spec.ts
+++ src/edge-shapes/shared/paths/cycle-square-edge-path/cycle-square-edge-path.spec.ts
@@ -48,6 +48,29 @@ describe("CycleSquareEdgePath", () => {
     );
   });
 
+  it("should create cycle square path with target arrow", () => {
+    const edgePath = new CycleSquareEdgePath({
+      from: {
+        coords: { x: 100, y: 100 },
+        dir: { x: 1, y: 0 },
+        hasArrow: false,
+      },
+      to: {
+        coords: { x: 100, y: 100 },
+        dir: { x: 1, y: 0 },
+        hasArrow: true,
+      },
+      side: 40,
+      arrowLength: 15,
+      arrowOffset: 5,
+      roundness: 10,
+    });
+
+    expect(edgePath.path).toBe(
+      "M 115 100 L 115 100 C 120 100 120 100 120 110 L 120 130 C 120 140 120 140 130 140 L 190 140 C 200 140 200 140 200 130 L 200 70 C 200 60 200 60 190 60 L 130 60 C 120 60 120 60 120 70 L 120 90 C 120 100 120 100 115 100 L 115 100",
+    );
+  });
+
   it("should calculate midpoint in between detour points", () => {
     const edgePath = new CycleSquareEdgePath({
       from: {
